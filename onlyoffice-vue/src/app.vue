f0ac252a21425a345ecaad4d2c9afdff93f0490f
gongzhen <517441540@qq.com>
2025-08-13 15:32:33 +0800

    feat: init

---
+++ onlyoffice-vue/src/app.vue
@@ -0,0 +1,33 @@
+<template>
+    <document-editor-jwt />
+</template>
+
+<script>
+import locale from 'ant-design-vue/lib/locale-provider/zh_CN'
+import DocumentEditorJwt from './views/onlyoffice/document-editor-jwt.vue'
+
+export default {
+  components: {
+    DocumentEditorJwt
+  },
+  data () {
+    return {
+      locale
+    }
+  }
+}
+</script>
+
+<style lang="less">
+body {
+  padding: 0;
+  margin: 0;
+}
+html,
+body {
+  font-size: 14px;
+  color: #333;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
+
+</style>
